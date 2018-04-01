21c6f76df3b5d287e083b33a25132f5cc6761d9b
iam2611008 <dragonwarrior1008@yandex.com>
2018-04-02 02:43:53 +0700

    create circle components binding with text input

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <input v-model="origin" placeholder="edit me">
+    <circle-container v-bind:height="100" v-bind:width="100" v-bind:text="origin"></circle-container>
+    <circle-container v-bind:height="200" v-bind:width="200" v-bind:text="origin"></circle-container>
+  </div>
+</template>
+
+<script>
+import Circle from './components/circle.vue';
+
+export default {
+  name: 'app',
+
+  components: {'circle-container': Circle},
+
+  data () {
+    return {
+      msg: 'Test project',
+      origin: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/Circle.vue
@@ -0,0 +1,35 @@
+<template>
+   <svg :height="height" :width="width">
+    <circle :cx="x(width)" :cy="y(height)" :r="calcR(width)" stroke="black" stroke-width="1" fill="#ddd" />
+    <text x="50%" y="50%" text-anchor="middle" dy=".3em">{{text}}</text>
+  </svg> 
+</template>
+
+<script>
+export default {
+  props: ['text', 'width', 'height'],
+
+  data () {
+    return {
+      child_msg: 'Hello From the Child!',
+      cx: 50,
+      cy: 50,
+      r: 40
+    }
+  },
+  
+  methods: {
+    x(width) {
+      return width/2;
+    },
+
+    y(height) {
+      return height/2;
+    },
+
+    calcR(width) {
+      return width/2 - 10;
+    }
+  }
+}
+</script>
